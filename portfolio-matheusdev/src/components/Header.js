997d7a7a5bde2b110183898642d199026f730885
matheussOliveira1 <matheus.aoliveira@sptech.school>
2024-04-05 02:05:28 -0300

    header and favicon created and configured

---
+++ portfolio-matheusdev/src/components/Header.js
@@ -0,0 +1,16 @@
+import "../App.css";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <a>Experiencias</a>
+        <a>Tecnologias</a>
+        <a>Projetos</a>
+        <a>Contato</a>
+      </header>
+    </>
+  );
+}
+
+export default Header;
